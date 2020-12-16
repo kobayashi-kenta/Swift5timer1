2a67c6b060588b2da1175a40ab9e25c6241a1dcb
kobayashi-kenta <12321klj@gmail.com>
2020-12-16 23:07:38 +0900

    Initial Commit

---
+++ Swift5timer1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5timer1
+//
+//  Created by Kenta on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
